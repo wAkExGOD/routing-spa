9d093645bd03293132e34a916b6261038e642ea2
Vlad Panasik <vladpanasik@mail.ru>
2024-05-09 19:06:03 +0300

    .gitignore fix

--- build/router.js
+++
@@ -1,9 +0,0 @@
-import { PAGES } from "./constants.js";
-const containerEl = document.querySelector(".container");
-export function router(url) {
-    if (!containerEl) {
-        return;
-    }
-    containerEl.innerHTML = "";
-    return PAGES.find((p) => p.check(url))?.render(url);
-}
