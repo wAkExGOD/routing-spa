d1bca7c60f1437a6a412ba2400611ca4463bcaba
Vlad Panasik <vladpanasik@mail.ru>
2024-05-12 01:43:27 +0300

    /build has been added

---
+++ build/router.js
@@ -0,0 +1,9 @@
+import { PAGES } from "./constants.js";
+const containerEl = document.querySelector(".container");
+export function router(url) {
+    if (!containerEl) {
+        return;
+    }
+    containerEl.innerHTML = "";
+    return PAGES.find((p) => p.check(url))?.render(url);
+}
